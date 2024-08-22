11c5bbf20eb2f4b53e1e303846b955074ce36616
Elynor Sykes <esykes@cherrycreekschools.org>
2024-08-22 19:32:49 +0000

    hi coke

--- SmallestLargest.java
+++ SmallestLargest.java
@@ -1,12 +1,36 @@
-//put your name here
+//Elynor Sykes
+
+import java.util.Random;
+
 public class SmallestLargest{
+    
+
     public static void main(String[] args){
+        int[] coke = new int[10];
+        Random rand = new Random();
+        for(int i=0; i<10;i++){
+            coke[i] = rand.nextInt(101)+1;
+        }
 
+        System.out.println("smallest value = " + smallestValue(coke));
+        System.out.println("highest value = " + largestValue(coke));
     }
-    public static int smallestValue(){
-
+    public static int smallestValue(int[] coke){
+        int smallestValue = coke[0];
+        for(int i=0; i<10; i++){
+            if(coke[i]<smallestValue){
+                smallestValue=coke[i];
+            }
+        }
+        return smallestValue;
     }
-    public static int largestValue(){
-        
+    public static int largestValue(int[] coke){
+        int highestValue = coke[0];
+        for(int i=0; i<10; i++){
+            if(coke[i]>highestValue){
+                highestValue=coke[i];
+            }
+        }
+        return highestValue;
     }
 }
